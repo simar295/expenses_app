d8e77ced8a0444daf9cddc582eb1194dc1f1dc77
simar295 <simarconnect@gmail.com>
2023-05-21 19:50:43 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ void main(List<String> args) {
+  runApp(screen());  
+}
+
+class screen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("hi"),
+    );
+  }
+}
